ef8647c950b6a70a253b5a412d402d7c41e90636
Curt Alfrey <79066679+curtalfrey@users.noreply.github.com>
2025-01-27 16:51:42 -0600

    Update forms.py
    Signed-off-by: Curt Alfrey <79066679+curtalfrey@users.noreply.github.com>

--- forms.py
+++ forms.py
@@ -1,18 +1,32 @@
-# forms.py
-
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField, HiddenField
-from wtforms.validators import DataRequired
+from wtforms import StringField, SubmitField, DateTimeField
+from wtforms.validators import DataRequired, ValidationError
 from datetime import datetime
 
+# Custom validators
+def validate_tool_id(form, field):
+    if len(field.data) != 8:  # Example: Tool ID must be 8 characters long
+        raise ValidationError('Tool ID must be exactly 8 characters long.')
+
+def validate_badge_id(form, field):
+    if not field.data.isalnum():  # Example: Badge ID must be alphanumeric
+        raise ValidationError('Badge ID must be alphanumeric.')
 
 class CheckInForm(FlaskForm):
-    tool_id_number = StringField('Tool ID', validators=[DataRequired()])
-    checkin_time = HiddenField('Check In Time', default=datetime.now().strftime('%Y-%m-%dT%H:%M'), validators=[DataRequired()])
+    tool_id_number = StringField('Tool ID', validators=[DataRequired(), validate_tool_id])
+    checkin_time = DateTimeField('Check In Time', format='%Y-%m-%dT%H:%M', validators=[DataRequired()])
     submit = SubmitField('Submit')
-    
+
+    def __init__(self, *args, **kwargs):
+        super(CheckInForm, self).__init__(*args, **kwargs)
+        self.checkin_time.data = datetime.now()
 
 class CheckOutForm(FlaskForm):
-    username_or_badge_id = StringField('Username or Badge ID', validators=[DataRequired()])
-    tool_id_number = StringField('Tool ID', validators=[DataRequired()])
-    checkout_time = HiddenField('Check Out Time', default=datetime.now().strftime('%Y-%m-%dT%H:%M'), validators=[DataRequired()])
+    username_or_badge_id = StringField('Username or Badge ID', validators=[DataRequired(), validate_badge_id])
+    tool_id_number = StringField('Tool ID', validators=[DataRequired(), validate_tool_id])
+    checkout_time = DateTimeField('Check Out Time', format='%Y-%m-%dT%H:%M', validators=[DataRequired()])
+    submit = SubmitField('Submit')
+
+    def __init__(self, *args, **kwargs):
+        super(CheckOutForm, self).__init__(*args, **kwargs)
+        self.checkout_time.data = datetime.now()
